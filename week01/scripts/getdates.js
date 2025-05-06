d83170689f8c3d188566f79159b7638aa203b98a
brayden0618 <bwayman@byupathway.edu>
2025-05-06 14:27:34 -0600

    Week 01 Assignment

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,13 @@
+const currentYear = document.querySelector("#currentYear");
+currentYear.innerHTML = new Date().getFullYear();
+const pattern = /(\d{1,2})\/(\d{1,2})\/(\d{4})/;
+if (
+    Date.parse(document.lastModified) >
+    (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
+) {
+    document.cookie = `lastModified=${document.lastModified}`;
+    const lastModified = document.querySelector("#lastModified");
+    lastModified.innerHTML = `Last modified: ${document.lastModified}`;
+}
+alert("This page was last modified on " + document.lastModified);
+const lastModified = document.querySelector("#lastModified");
